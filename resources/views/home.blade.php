292ce464bb52dc0e4c25346fdbbdd4274cf3746d
DavideCavallotto <davidecavallotto92@gmail.com>
2023-12-07 15:48:35 +0100

    ADD dinamic text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,12 @@
-<h1>Hello Everybody</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravello</title>
+</head>
+<body>
+    <p>Mi chiamo {{ $name }} {{ $surname }} e sono {{ $age }}</p>
+    
+</body>
+</html>
